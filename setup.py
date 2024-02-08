9148794aaff9a35bfacfac780c133e9fe9d83f58
Julian Nguyen <109386615+juliannguyen4@users.noreply.github.com>
2024-02-07 16:40:04 -0800

    First attempt

--- setup.py
+++ setup.py
@@ -260,12 +260,11 @@ setup(
     ext_modules=[
         Extension(
             # Extension Name
-            'aerospike',
+            'aerospike.aerospike',
 
             # Source Files
             [
                 'src/main/aerospike.c',
-                'src/main/exception.c',
                 'src/main/log.c',
                 'src/main/client/type.c',
                 'src/main/client/apply.c',
@@ -355,6 +354,21 @@ setup(
             libraries=libraries,
             extra_objects=extra_objects,
             extra_link_args=extra_link_args,
+        ),
+        Extension(
+            name="aerospike.exception",
+            sources=[
+                'src/main/exception.c'
+            ],
+            # Compile
+            include_dirs=include_dirs,
+            extra_compile_args=extra_compile_args,
+
+            # Link
+            library_dirs=library_dirs,
+            libraries=libraries,
+            extra_objects=extra_objects,
+            extra_link_args=extra_link_args,
         )
     ],
     package_data={
@@ -367,7 +381,8 @@ setup(
     },
     packages=['aerospike_helpers', 'aerospike_helpers.operations', 'aerospike_helpers.batch',
               'aerospike_helpers.expressions',
-              'aerospike-stubs'],
+              'aerospike-stubs',
+              'aerospike'],
     cmdclass={
         'build': CClientBuild,
         'clean': CClientClean
